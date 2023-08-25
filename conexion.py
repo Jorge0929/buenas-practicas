adf736f579af76510ac74f4d780d0c438bc73b64
Jorge0929 <129603851+Jorge0929@users.noreply.github.com>
2023-08-25 10:39:41 -0500

    consulta Estudiantes

--- conexion.py
+++ conexion.py
@@ -1,14 +1,12 @@
 import sys
 import pyodbc
-conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ= C:\Users\jorge\OneDrive\Documentos\universidad\Buenas practicas de desarrollo\Base de datos acces\base_da.accdb;")
+conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\jorge\OneDrive\Documentos\universidad\Buenas practicas de desarrollo\Base de datos acces\Database2.accdb;")
 cursor=conn.cursor()
 cursor.execute("SELECT * FROM Estudiante WHERE edad>20")
 for row in cursor.fetchall():
     print(row)
 cursor.close
 conn.close
-
-
 
 
 
