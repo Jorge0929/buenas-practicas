ba1e92fd5bd9df0ed48427240f984f7c36b8ecdf
Jorge0929 <129603851+Jorge0929@users.noreply.github.com>
2023-08-25 10:27:55 -0500

    consulta en la tabla Estudiante

--- conexion.py
+++ conexion.py
@@ -2,7 +2,7 @@ import sys
 import pyodbc
 conn= pyodbc.connect(r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ= C:\Users\jorge\OneDrive\Documentos\universidad\Buenas practicas de desarrollo\Base de datos acces\base_da.accdb;")
 cursor=conn.cursor()
-cursor.execute("SELECT * FROM Registro_notas WHERE Nota>4.5")
+cursor.execute("SELECT * FROM Estudiante WHERE edad>20")
 for row in cursor.fetchall():
     print(row)
 cursor.close
